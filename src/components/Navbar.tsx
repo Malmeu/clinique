85a44439af4951f1cd03eb16a1da99a65b783c3c
Malmeu <darkvid3r@gmail.com>
2025-04-13 13:47:08 +0100

    Correction des erreurs ESLint pour le du00e9ploiement sur Vercel

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import Link from 'next/link';
-import Image from 'next/image';
 import { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import { FiMenu, FiX } from 'react-icons/fi';
